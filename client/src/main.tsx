780d2a0ac0b54886fd1edca81d1d85064e315e93
Jamie Degnan <jamie.degnan@gmail.com>
2025-11-14 12:11:10 -0800

    Add default fonts

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ApolloProvider } from "@apollo/client/react";
-import { client } from "./apollo/client";
+import { client } from "@/apollo/client";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { appTheme } from "@/theme/appTheme.ts";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <ThemeProvider theme={appTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </ApolloProvider>
   </StrictMode>
 );
---
+++ client/src/theme/appTheme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+
+export const appTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+  typography: {
+    fontFamily:
+      '"Mona Sans", "Instrument Sans", "Helvetica", "Arial", sans-serif',
+  },
+});
