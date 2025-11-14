6b146a02e2db98d73e6e251bf8c390a6f0b360a6
Jamie Degnan <jamie.degnan@gmail.com>
2025-11-13 20:26:57 -0800

    small css changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -9,15 +9,13 @@ function App() {
   const [count, setCount] = useState(0);
   const { data } = useQuery(GET_USER);
 
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
-
   return (
-    <>
-      <div className="data-example">
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      </div>
+    <div className="app-container">
+      {data && (
+        <div className="data-example">
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      )}
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -38,7 +36,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
 
