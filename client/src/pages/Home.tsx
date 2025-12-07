305fbd900595be165b5fa48d8d4c811b86c561f9
Hristo Karamanliev <karamanliev@gmail.com>
2025-12-07 16:16:45 +0200

    test data fetching in home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,41 @@
 import PageLayout from "@/components/layout/PageLayout";
+import { useSpace } from "@/context/SpaceContext";
+import { GET_METRICS } from "@/graphql/queries/GetMetrics";
+import { GET_RECOMMENDATIONS } from "@/graphql/queries/GetRecommendations";
+import { GET_TICKETS } from "@/graphql/queries/GetTickets";
+import { useQuery } from "@apollo/client/react";
 
 function Home() {
+  const { currentSpaceId } = useSpace();
+
+  const { data: recommendationsData, loading: isRecommendationsLoading } =
+    useQuery(GET_RECOMMENDATIONS, {
+      variables: {
+        spaceId: currentSpaceId!,
+      },
+      skip: !currentSpaceId,
+    });
+
+  const { data: metricsData, loading: isMetricsLoading } = useQuery(
+    GET_METRICS,
+    {
+      variables: {
+        spaceId: currentSpaceId!,
+      },
+      skip: !currentSpaceId,
+    },
+  );
+
+  const { data: ticketsData, loading: isTicketsLoading } = useQuery(
+    GET_TICKETS,
+    {
+      variables: {
+        spaceId: currentSpaceId!,
+      },
+      skip: !currentSpaceId,
+    },
+  );
+
   return (
     <PageLayout
       title="Home"
@@ -8,7 +43,30 @@ function Home() {
         <button onClick={() => console.log("btn")}>Button</button>
       }
     >
-      <div>Hello world</div>
+      <div>
+        <h2>Recommendations</h2>
+        {isRecommendationsLoading ? (
+          <div>Recommendations Loading...</div>
+        ) : (
+          <div>{JSON.stringify(recommendationsData)}</div>
+        )}
+      </div>
+      <div>
+        <h2>Metrics</h2>
+        {isMetricsLoading ? (
+          <div>Metrics Loading...</div>
+        ) : (
+          <div>{JSON.stringify(metricsData)}</div>
+        )}
+      </div>
+      <div>
+        <h2>Tickets</h2>
+        {isTicketsLoading ? (
+          <div>Tickets Loading...</div>
+        ) : (
+          <div>{JSON.stringify(ticketsData)}</div>
+        )}
+      </div>
     </PageLayout>
   );
 }
