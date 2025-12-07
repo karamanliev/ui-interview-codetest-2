efb5ae6aad5d8df3f59d93caff114ebcedf2ecc0
Hristo Karamanliev <karamanliev@gmail.com>
2025-12-07 06:09:03 +0200

    add route code splitting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,22 @@
 import DashboardLayout from "@/components/layout/DashboardLayout";
-import { Home, Tasks, Team } from "@/pages";
 import { Route, Routes } from "react-router";
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => import("@/pages/Home"));
+const Tasks = lazy(() => import("@/pages/Tasks"));
+const Team = lazy(() => import("@/pages/Team"));
 
 function App() {
   return (
-    <Routes>
-      <Route element={<DashboardLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/tasks" element={<Tasks />} />
-        <Route path="/team/:teamId" element={<Team />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>Loading page...</div>}>
+      <Routes>
+        <Route element={<DashboardLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/team/:teamId" element={<Team />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
--- client/src/pages/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as Home } from "./Home";
-export { default as Tasks } from "./Tasks";
-export { default as Team } from "./Team";
