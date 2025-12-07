72acbe889de64e29ff60a8176c8b6295c9709ace
Hristo Karamanliev <karamanliev@gmail.com>
2025-12-07 03:18:36 +0200

    add SpaceContext

---
+++ client/src/context/SpaceContext.tsx
@@ -0,0 +1,39 @@
+import {
+  createContext,
+  useContext,
+  useState,
+  type PropsWithChildren,
+} from "react";
+import { useQuery } from "@apollo/client/react";
+import { GET_USER } from "@/graphql/queries/GetUser";
+import type { Space } from "@/types/graphql";
+
+type SpaceContextType = {
+  currentSpaceId: string | null;
+  setCurrentSpaceId: React.Dispatch<React.SetStateAction<string | null>>;
+  spaces: Space[];
+};
+
+const SpaceContext = createContext<SpaceContextType | undefined>(undefined);
+
+export function SpaceProvider({ children }: PropsWithChildren) {
+  const { data } = useQuery(GET_USER);
+  const spaces = data?.user?.spaces || [];
+
+  const [selectedSpaceId, setSelectedSpaceId] = useState<string | null>(null);
+  const currentSpaceId = selectedSpaceId ?? spaces[0]?.id;
+
+  return (
+    <SpaceContext.Provider
+      value={{
+        currentSpaceId,
+        setCurrentSpaceId: setSelectedSpaceId,
+        spaces,
+      }}
+    >
+      {children}
+    </SpaceContext.Provider>
+  );
+}
+
+export const useSpace = () => useContext(SpaceContext);
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import { ApolloProvider } from "@apollo/client/react";
 import { BrowserRouter } from "react-router";
 import { client } from "@/apollo/client";
 import { ThemeProvider } from "@mui/material/styles";
+import { SpaceProvider } from "./context/SpaceContext.tsx";
 import CssBaseline from "@mui/material/CssBaseline";
 import { appTheme } from "@/theme/appTheme.ts";
 import "./index.css";
@@ -15,7 +16,9 @@ createRoot(document.getElementById("root")!).render(
       <ThemeProvider theme={appTheme}>
         <CssBaseline />
         <BrowserRouter>
-          <App />
+          <SpaceProvider>
+            <App />
+          </SpaceProvider>
         </BrowserRouter>
       </ThemeProvider>
     </ApolloProvider>
